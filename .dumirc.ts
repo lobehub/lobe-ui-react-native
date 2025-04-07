000dedf058c4b251f0523a1c944f4713227e9b34
rdmclin2 <rdmclin2@gmail.com>
2025-04-07 22:27:18 +0800

    fix: babel compile error

--- .dumirc.ts
+++ .dumirc.ts
@@ -9,4 +9,32 @@ export default defineConfig({
     'react-native$': 'react-native-web',
     'react-native-svg$': 'react-native-svg-web',
   },
+  extraBabelPresets: ['@babel/preset-react'],
+  extraBabelPlugins: ['@babel/plugin-transform-react-jsx'],
+  chainWebpack(memo) {
+    memo.module
+      .rule('js')
+      .include.add(/react-native-markdown-display/)
+      .add(/react-native-mathjax-html-to-svg/)
+      .add(/react-native-shiki-engine/)
+      .end()
+      .use('babel-loader')
+      .loader('babel-loader')
+      .options({
+        presets: [
+          '@babel/preset-react',
+          [
+            '@babel/preset-env',
+            {
+              modules: false,
+              targets: {
+                esmodules: true,
+              },
+            },
+          ],
+        ],
+        plugins: ['@babel/plugin-transform-react-jsx'],
+      });
+    return memo;
+  },
 });
